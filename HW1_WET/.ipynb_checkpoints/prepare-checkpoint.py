03de5dd65985b3d01f2faa8b6ca658fda35252bc
HssainAzaqy135 <kinghssain135@gmail.com>
2024-06-29 17:57:59 +0300

    Finished Everything , final touches before submitting

--- HW1_WET/.ipynb_checkpoints/prepare-checkpoint.py
+++ HW1_WET/.ipynb_checkpoints/prepare-checkpoint.py
@@ -1,33 +1,47 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 def prepare_data(training_data, new_data):
     new_copy = new_data.copy()
+    # removing blood_type and adding special_property
+    new_copy["SpecialProperty"] = new_copy["blood_type"].isin(["O+", "B+"])
+    new_copy.drop(columns=["blood_type"], inplace=True)
+    
     # Splitting PCR features
-    all_features = training_data.columns.tolist()
-    
+    all_features = new_data.columns.tolist() 
     pcr_features = [feature for feature in all_features if feature.startswith("PCR")]
+    list_to_standard_scale = [feature for feature in pcr_features if (pcr_features.index(feature)+1) in [1,2,4,5,6,7,8,9]]
+    list_to_min_max = [feature for feature in pcr_features if (pcr_features.index(feature)+1) in [3,10]]
+
+    # features to scale
+    features_to_min_max = new_data[list_to_min_max]
+    features_to_standard_scale = new_data[list_to_standard_scale]
     
-    features_to_standard_scale = [feature for feature in pcr_features if pcr_features.index(feature) in [5,6,8]]
-    features_to_min_max = [feature for feature in pcr_features if pcr_features.index(feature) in [1,2,3,4,7,9,10]]
-
-    pcr_training_DF  = training_data[features_to_standard_scale]
-    pcr_new_DF = new_copy[features_to_min_max]
+    # features to fit the scalers
+    min_max_fit_data = training_data[list_to_min_max]
+    standard_scale_fit_data = training_data[list_to_standard_scale]
     
-    # Min max Scaling
-    max = 1
-    min = -1
-    min_maxed_features = (pcr_new_DF_ - pcr_new_DF.min(axis=0)) / (pcr_new_DF.max(axis=0) - pcr_new_DF.min(axis=0)) * (max - min) + min
-    for feature in features_to_min_max:
-        new_copy[feature] = min_maxed_features[feature]
+    # init scalers
+    min_max_scaler = MinMaxScaler(feature_range=(-1, 1))
+    standard_scaler = StandardScaler()
+    
+    # Fitting scalers to training data
+    min_max_scaler.fit(min_max_fit_data)
+    standard_scaler.fit(standard_scale_fit_data)
+    
+    # scaling
+    min_max_scaled_features = pd.DataFrame(min_max_scaler.transform(features_to_min_max),
+                                           columns=list_to_min_max,
+                                           index=new_copy.index)
+    standard_scaled_features = pd.DataFrame(standard_scaler.transform(features_to_standard_scale),
+                                            columns=list_to_standard_scale,
+                                            index=new_copy.index)
+    
+    # updating the needed dataframe
+    new_copy.update(min_max_scaled_features)
+    new_copy.update(standard_scaled_features)
 
-
-    # Standard Scaling
-    stds = pcr_training_DF.std()
-    means = pcr_training_DF.mean()
-    standardized_features = (pcr_new_DF-means)/stds
-    for feature in features_to_standard_scale:
-        new_copy[feature] = standardized_features[feature]
 
     return new_copy
 
